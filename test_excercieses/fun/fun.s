76b64299f47c45be0d65bf178dfc4f0f87921080
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-06-13 10:28:06 +0200

    update

---
+++ test_excercieses/fun/fun.s
@@ -0,0 +1,44 @@
+# miejsce na zmienne
+.data 
+
+# miejsce na kod
+    .type fun, @function # deklaracja funkcji
+    .global fun # deklarujemy nasza funkcje jako globalna(punkt startowy programu)
+
+.text
+
+fun:
+    #rdi - buf, rsi - a, rdx - b
+    mov     %rdi, %rax
+    
+pierwszy:
+    mov     (%rsi), %bl
+    mov     %bl, (%rdi)
+    inc     %rdi
+    inc     %rsi
+    
+drugi:
+    mov     (%rdx), %bl
+    mov     %bl, (%rdi)
+    inc     %rdi
+    inc     %rdx
+    
+porownaj:
+    cmp     $0, (%rsi)
+    je      porownajDrugi
+    cmp     $0, (%rdx)
+    je      koniec
+    jmp     pierwszy
+    
+# porownajPierwszy:
+#    cmp     $0, (%rdx)
+#    je porownajDrugi
+    
+porownajDrugi:
+    cmp     $0, (%rsi)
+    je      koniec
+    jmp     drugi
+    
+koniec:    
+	ret	    # wyjscie z funkcji
+    
